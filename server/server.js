308d280b27cb227ab2a62112299f5e0879a59ad9
PRESTEBAN <145607969+PRESTEBAN@users.noreply.github.com>
2024-08-21 20:40:57 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -40,7 +40,7 @@ app.post('/send-notification', async (req, res) => {
       notification: {
         title: 'Nueva foto',
         body: message,
-        image: 'https://i.ibb.co/HLvMS2Z/formulario.jpg',
+        imageUrl: 'https://i.ibb.co/HLvMS2Z/formulario.jpg',
       },
       token: token,
     };
